a2cbbf71f62d4033fd3692ddddd3ba7459974aa2
Les Hill <leshill@gmail.com>
2011-11-30 00:19:22 -0800

    Call another step with #step

--- lib/turnip/example_group.rb
+++ lib/turnip/example_group.rb
@@ -2,7 +2,7 @@ module Turnip
   module ExampleGroup
     def execute_steps(steps)
       steps.each do |step|
-        Turnip::StepDefinition.execute(self, available_steps, step)
+        execute_step(step)
       end
     end
 
@@ -10,8 +10,20 @@ module Turnip
       self.available_steps = Turnip::StepModule.all_steps_for(*tags)
     end
 
+    def step(description)
+      execute_step(build_step(description))
+    end
+
     private
 
     attr_accessor :available_steps
+
+    def build_step(description)
+      Turnip::Builder::Step.new(description, nil)
+    end
+
+    def execute_step(step)
+      Turnip::StepDefinition.execute(self, available_steps, step)
+    end
   end
 end
--- spec/example_group_spec.rb
+++ spec/example_group_spec.rb
@@ -3,6 +3,13 @@ require 'turnip/example_group'
 describe Turnip::ExampleGroup do
   include Turnip::ExampleGroup
 
+  describe '#build_step' do
+    it 'builds a new step from the description' do
+      Turnip::Builder::Step.should_receive(:new).with('description', nil)
+      build_step('description')
+    end
+  end
+
   describe '#execute_steps' do
     before { @available_steps = available_steps }
 
@@ -25,6 +32,22 @@ describe Turnip::ExampleGroup do
       Turnip::StepModule.should_receive(:all_steps_for).with(tag1, tag2).and_return(steps)
       load_steps_for(tag1, tag2)
       @available_steps.should eq(steps)
+    end
+  end
+
+  describe '#step' do
+    before { @available_steps = available_steps }
+
+    let(:available_steps) { stub }
+    let(:built_step) { stub }
+
+    def build_step(description)
+      built_step
+    end
+
+    it 'executes a newly built step in the context' do
+      Turnip::StepDefinition.should_receive(:execute).with(self, available_steps, built_step)
+      step('description')
     end
   end
 end
